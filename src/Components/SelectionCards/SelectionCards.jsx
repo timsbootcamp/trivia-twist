cde37892d090b9f1d474ce4f5f04d0e4785d90e8
Estherbab <153003348+Estherbab@users.noreply.github.com>
2024-03-15 21:26:06 +0000

    Added imports for folders & function for selection cards

---
+++ src/Components/SelectionCards/SelectionCards.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+import TriviaSelection from '../TriviaSelection/TriviaSelection';
+import './SelectionCards.css'
+
+function SelectionCards() {
+
+    return  (
+        <></>
+    )
+
+}
+
+export default SelectionCards;
