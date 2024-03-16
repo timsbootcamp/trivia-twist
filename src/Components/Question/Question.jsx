85974cc3b3b2fb23d9e479037bbd265c9a2a4964
nicosharpe <nicolesharpe11@gmail.com>
2024-03-16 22:43:52 +0000

    added question components, need click events

--- src/Components/Question/Question.jsx
+++ src/Components/Question/Question.jsx
@@ -1,8 +1,59 @@
-import React, { useState } from 'react';
-import AnswerOption from '../AnswerOption/AnswerOption'; 
-import QuestionContainer from '../Question-Container/QuestionContainer';
-import './index.css'
+// import React, { useState } from 'react';
+// import AnswerOption from '../AnswerOption/AnswerOption'; 
+// import QuestionContainer from '../Question-Container/QuestionContainer';
+// import './index.css'
+// import { decodeHTMLEntities } from "../../Lib/formatting";
+// import 'bulma/css/bulma.min.css';
 
+
+
+
+// const Question = ({ questionData, onAnswer, onNext }) => {
+//   const [selectedOption, setSelectedOption] = useState('');
+
+//   const handleOptionSelect = (option) => {
+//     setSelectedOption(option);
+//   };
+
+//   const handleSubmit = () => {
+//     onAnswer(selectedOption);
+//     setSelectedOption('');
+//     onNext();
+//   };
+
+//   return (
+//     <>
+
+// <div style={{display: 'block'}}>
+//   <Notification color="info">
+//     <div className="Question">{decodeHTMLEntities(questionData.question)}</div>
+//   </Notification>
+// </div>
+
+
+//   <ul>
+//   {questionData.options.map((option, index) => (
+//     <AnswerOption
+//       key={index}
+//       option={option}
+//       selected={selectedOption === option}
+//       onSelect={handleOptionSelect}
+//     />
+//   ))}
+// </ul>
+// <button onClick={handleSubmit}>Submit Answer</button>
+// </>
+// );
+// };
+
+// export default Question;
+
+import React, { useState } from 'react';
+import AnswerOption from '../AnswerOption/AnswerOption';
+import QuestionContainer from '../Question-Container/QuestionContainer';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { decodeHTMLEntities } from "../../Lib/formatting";
 
 const Question = ({ questionData, onAnswer, onNext }) => {
   const [selectedOption, setSelectedOption] = useState('');
@@ -19,23 +70,35 @@ const Question = ({ questionData, onAnswer, onNext }) => {
 
   return (
     <>
-  <QuestionContainer>
+   <div style={{ display: 'block', border: '1px solid black', backgroundColor:'#D0FF96', padding: '10px', marginBottom: '20px' }}>
+   <div className="Question">{questionData.question}</div>
+   {/* <div className="Question">{decodeHTMLEntities(questionData.question)}</div> */}
 
-</QuestionContainer>
-<text><h2 className="Question">{questionData.question}</h2></text>
-  <ul>
-  {questionData.options.map((option, index) => (
-    <AnswerOption
-      key={index}
-      option={option}
-      selected={selectedOption === option}
-      onSelect={handleOptionSelect}
-    />
-  ))}
-</ul>
-<button onClick={handleSubmit}>Submit Answer</button>
-</>
-);
+      </div>
+
+      <div>
+        {questionData.options.map((option, index) => (
+          <div key={index}>
+            <div style={{ display: 'block', border: '1px solid black', backgroundColor:'#07B4AA', padding: '10px', marginBottom: '10px' }} onClick={() => handleOptionSelect(option)}>
+              {decodeHTMLEntities(option)}
+            </div>
+          </div>
+        ))}
+      </div>
+      {/* <ul>
+        {questionData.options.map((option, index) => (
+          <AnswerOption
+            key={index}
+            option={option}
+            selected={selectedOption === option}
+            onSelect={handleOptionSelect}
+          />
+        ))}
+      </ul>
+      <button onClick={handleSubmit}>Submit Answer</button> */}
+    </>
+  );
 };
 
 export default Question;
+
--- src/Components/TriviaSelection/TriviaSelection.jsx
+++ src/Components/TriviaSelection/TriviaSelection.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
-import SelectionCards from '../SelectionCards/SelectionCards';
+import React, { useState } from "react";
+import SelectionCards from "../SelectionCards/SelectionCards";
+import 'bulma/css/bulma.min.css';
 
 const generateTriviaUrl = (category, difficulty) => {
   return `https://opentdb.com/api.php?amount=10&category=${category}&difficulty=${difficulty}&type=multiple`;
@@ -7,17 +8,17 @@ const generateTriviaUrl = (category, difficulty) => {
 
 const TriviaSelection = ({ onSelectDifficulty }) => {
   const triviaCategories = {
-    "Animals": 27,
+    Animals: 27,
     "Entertainment: Music": 12,
     "General Knowledge": 9,
     "Science & Nature": 17,
-    "Sports": 21
+    Sports: 21,
   };
 
-  const difficulties = ['Easy', 'Medium', 'Hard'];
+  const difficulties = ["Easy", "Medium", "Hard"];
 
-  const [selectedCategory, setSelectedCategory] = useState('');
-  const [selectedDifficulty, setSelectedDifficulty] = useState('');
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const [selectedDifficulty, setSelectedDifficulty] = useState("");
   const [quizStarted, setQuizStarted] = useState(false);
 
   const handleSelectCategory = (event) => {
@@ -32,42 +33,75 @@ const TriviaSelection = ({ onSelectDifficulty }) => {
 
   const handleStartQuiz = () => {
     if (selectedCategory && selectedDifficulty) {
-      onSelectDifficulty(generateTriviaUrl(triviaCategories[selectedCategory], selectedDifficulty.toLowerCase()));
+      onSelectDifficulty(
+        generateTriviaUrl(
+          triviaCategories[selectedCategory],
+          selectedDifficulty.toLowerCase()
+        )
+      );
       setQuizStarted(true);
     } else {
       console.error("Please select both category and difficulty.");
     }
   };
 
-  const titleText = selectedCategory && selectedDifficulty ? `${selectedCategory} (${selectedDifficulty}) Quiz` : "Trivia Quiz";
+  const titleText =
+    selectedCategory && selectedDifficulty
+      ? `${selectedCategory} (${selectedDifficulty}) Quiz`
+      : "Trivia Quiz";
 
   return (
     <div>
-
       <h1>{titleText}</h1>
 
       {!selectedCategory && (
         <div>
           <h2>Select Trivia Category:</h2>
           <SelectionCards></SelectionCards>
-          <select value={selectedCategory} onChange={handleSelectCategory}>
+          <select
+            className="select"
+            value={selectedCategory}
+            onChange={handleSelectCategory}
+          >
+            <option value="">Select Category</option>
+            {Object.keys(triviaCategories).map((category) => (
+              <option key={category} value={category}>
+                {category}
+              </option>
+            ))}
+          </select>
+          {/* <select value={selectedCategory} onChange={handleSelectCategory}>
             <option value="">Select Category</option>
             {Object.keys(triviaCategories).map((category) => (
               <option key={category} value={category}>{category}</option>
             ))}
-          </select>
+          </select> */}
         </div>
       )}
 
       {selectedCategory && !selectedDifficulty && (
         <div>
           <h2>Select Trivia Difficulty:</h2>
-          <select value={selectedDifficulty} onChange={handleSelectDifficulty}>
+          <select
+            className="select"
+            value={selectedDifficulty}
+            onChange={handleSelectDifficulty}
+          >
             <option value="">Select Difficulty</option>
             {difficulties.map((difficulty) => (
-              <option key={difficulty} value={difficulty}>{difficulty}</option>
+              <option key={difficulty} value={difficulty}>
+                {difficulty}
+              </option>
             ))}
           </select>
+          {/* <select value={selectedDifficulty} onChange={handleSelectDifficulty}>
+            <option value="">Select Difficulty</option>
+            {difficulties.map((difficulty) => (
+              <option key={difficulty} value={difficulty}>
+                {difficulty}
+              </option>
+            ))}
+          </select> */}
         </div>
       )}
 
@@ -78,7 +112,6 @@ const TriviaSelection = ({ onSelectDifficulty }) => {
           </button>
         </div>
       )}
-
     </div>
   );
 };
