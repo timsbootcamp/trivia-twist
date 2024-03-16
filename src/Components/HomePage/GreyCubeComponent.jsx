94c6a019ae26678e81aae69d9a0d1a79a4c8da46
LeonieWhitehead <leoniewhitehead@hotmail.co.uk>
2024-03-16 11:45:00 +0000

    added animation code for grey cube

--- src/Components/HomePage/GreyCubeComponent.jsx
+++ src/Components/HomePage/GreyCubeComponent.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React , { useEffect }from "react";
+import anime from "animejs/lib/anime.es.js";
 import GreyCube from "../../assets/GreyCube.svg";
 
 export const GreyCubeComponent = () => {
+ useEffect(() => {
+  anime({
+    targets: ".greyCube",
+    translateY: [-50, 50],
+    duration: 2000, 
+    loop: true,
+    easing: 'easeInOutSine',
+    direction: 'alternate'
+  });
+}, []);
+
   return (
     <div className="greyCubeContainer">
       <img src={GreyCube} alt="GreyCube" />
