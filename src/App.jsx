4f0d55c8491c64fa540359855a14cb8931501250
nicosharpe <nicolesharpe11@gmail.com>
2024-03-16 22:43:40 +0000

    added background and question components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import HomePage from './Components/HomePage/HomePage.jsx'
+import './App.css'
 
 function App() {
   return (
+    <div className='app'>
     <HomePage />
+    
+    </div>
   );
 }
 
--- src/Components/Question/Question.jsx
+++ src/Components/Question/Question.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import AnswerOption from '../AnswerOption/AnswerOption'; 
-import { decodeHTMLEntities } from "../../Lib/formatting";
+import QuestionContainer from '../Question-Container/QuestionContainer';
+import './index.css'
+
 
 const Question = ({ questionData, onAnswer, onNext }) => {
   const [selectedOption, setSelectedOption] = useState('');
@@ -16,21 +18,24 @@ const Question = ({ questionData, onAnswer, onNext }) => {
   };
 
   return (
-    <div>
-      <h2>{decodeHTMLEntities(questionData.question)}</h2>
-      <ul>
-        {questionData.options.map((option, index) => (
-          <AnswerOption
-            key={index}
-            option={option}
-            selected={selectedOption === option}
-            onSelect={handleOptionSelect}
-          />
-        ))}
-      </ul>
-      <button onClick={handleSubmit}>Submit Answer</button>
-    </div>
-  );
+    <>
+  <QuestionContainer>
+
+</QuestionContainer>
+<text><h2 className="Question">{questionData.question}</h2></text>
+  <ul>
+  {questionData.options.map((option, index) => (
+    <AnswerOption
+      key={index}
+      option={option}
+      selected={selectedOption === option}
+      onSelect={handleOptionSelect}
+    />
+  ))}
+</ul>
+<button onClick={handleSubmit}>Submit Answer</button>
+</>
+);
 };
 
 export default Question;
