24e67caeedc907e3a0e0d0a8e58fef70c11c59b7
nicosharpe <nicolesharpe11@gmail.com>
2024-03-18 16:28:00 +0000

    created styling for category and question pages

--- src/Components/Question/Question.jsx
+++ src/Components/Question/Question.jsx
@@ -1,46 +1,95 @@
-import React, { useState } from 'react';
-import AnswerOption from '../AnswerOption/AnswerOption';
-import QuestionContainer from '../Question-Container/QuestionContainer';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import React, { useState } from "react";
+import AnswerOption from "../AnswerOption/AnswerOption";
 import { decodeHTMLEntities } from "../../Lib/formatting";
+import "bulma/css/bulma.min.css";
+import "./index.css";
+import { Container } from "react-bulma-components";
 
 const Question = ({ questionData, onAnswer, onNext }) => {
-  const [selectedOption, setSelectedOption] = useState('');
+  const [selectedOption, setSelectedOption] = useState("");
 
   const handleOptionSelect = (option) => {
     setSelectedOption(option);
     onAnswer(option); // Pass the selected option back to QuizApp
     onNext(); // Move to the next question
   };
-  
 
   const handleSubmit = () => {
     onAnswer(selectedOption);
-    setSelectedOption('');
+    setSelectedOption("");
     onNext();
   };
 
-
-
   return (
     <>
-   <div style={{ display: 'block', border: '1px solid black', backgroundColor:'#D0FF96', padding: '10px', marginBottom: '20px' }}>
-   <div className="Question">{decodeHTMLEntities(questionData.question)}</div>
+      <img
+        className="BgGraphics"
+        src="src\assets\Elements\Categories-Page-Elements\2D-BG-Elemnts.png"
+      ></img>
 
+      <div class="columns">
+        <div class="column logoIcon">
+          <img src="src\assets\Elements\Categories-Page-Elements\Logo.png"></img>
+        </div>
       </div>
 
-      <div>
-        {questionData.options.map((option, index) => (
-          <div key={index} onClick={() => handleOptionSelect(option)} style={{ display: 'block', border: '1px solid black', padding: '10px', marginBottom: '10px', backgroundColor: '#FFF', cursor: 'pointer' }}>
-            {decodeHTMLEntities(option)}
+      <div class="columns">
+        <div class="column is-one-third">
+          <img
+            className="cubes"
+            src="src\assets\Elements\Categories-Page-Elements\left side cubes.svg"
+          ></img>
+        </div>
+
+
+      <Container className="questionContainer">
+        <div class="columns">
+          <div class="column is-full">
+            <div
+              style={{
+                display: "block",
+                border: "1px solid black",
+                backgroundColor: "#D0FF96",
+                padding: "10px",
+                marginBottom: "20px",
+              }}
+            >
+              <div className="Question">
+                {decodeHTMLEntities(questionData.question)}
+              </div>
+            </div>
+
+            <div className="Options">
+              {questionData.options.map((option, index) => (
+                <div
+                  key={index}
+                  onClick={() => handleOptionSelect(option)}
+                  style={{
+                    display: "block",
+                    border: "1px solid black",
+                    padding: "10px",
+                    marginBottom: "10px",
+                    backgroundColor: "#FFF",
+                    cursor: "pointer",
+                  }}
+                >
+                  {decodeHTMLEntities(option)}
+                </div>
+              ))}
+            </div>
           </div>
-        ))}
+        </div>
+      </Container>
+
+      <div class="column is-one-third">
+        <img
+          className="cubes"
+          src="src\assets\Elements\Categories-Page-Elements\right side cubes.svg"
+        ></img>
       </div>
-    
+      </div>
     </>
   );
 };
 
 export default Question;
-
