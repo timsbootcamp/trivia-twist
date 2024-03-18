0f6d1f736a9bd64e7f328edd74e494df5159629c
Estherbab <153003348+Estherbab@users.noreply.github.com>
2024-03-18 11:49:53 +0000

    Added hover colours to cards

--- src/Components/LevelHardCard/HardLevel.jsx
+++ src/Components/LevelHardCard/HardLevel.jsx
@@ -8,7 +8,7 @@ const HardCard = ({ SelectedDifficulty }) => {
   };
 
   return (
-    <button className='Easy mt-5 mb-5 col-sm-12 col-md-4' onClick={handleSelectDifficulty}>
+    <button className='Hard mt-5 mb-5 col-sm-12 col-md-4' onClick={handleSelectDifficulty}>
  <svg
     xmlns="http://www.w3.org/2000/svg"
     width={399}
