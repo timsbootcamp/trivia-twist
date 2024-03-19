66f481c80ac08a8f1436aae1849d2a660826c62d
Obaid Gulzar <obaid3337@googlemail.com>
2024-03-19 22:39:10 +0000

    click sounds play when starting teh game and going back to hoem page

--- src/Components/HomePage/HomePage.jsx
+++ src/Components/HomePage/HomePage.jsx
@@ -1,24 +1,26 @@
 import React, { useState } from 'react';
+import useSound from 'use-sound';
 import "./HomePage.css";
-import TriviaSelection from '../TriviaSelection/TriviaSelection'
+import TriviaSelection from '../TriviaSelection/TriviaSelection';
 import QuizApp from '../QuizApp/QuizApp';
-
+import ClickSound from "../../Sounds/ButtonClickSound.wav";
 
 function HomePage() {
- const [isHomePageVisible, setIsHomePageVisible] = useState(true);
+  const [isHomePageVisible, setIsHomePageVisible] = useState(true);
 
- const handleStartClick = () => {
+  const handleStartClick = () => {
     setIsHomePageVisible(false);
- };
- 
- const [selectedUrl, setSelectedUrl] = useState(null);
+  };
 
- const handleSelectDifficulty = (url) => {
-   setSelectedUrl(url);
- };
+  const [selectedUrl, setSelectedUrl] = useState(null);
 
+  const handleSelectDifficulty = (url) => {
+    setSelectedUrl(url);
+  };
 
- return (
+  const [clickSound] = useSound(ClickSound);
+
+  return (
     <div className="homePage">
       {isHomePageVisible ? (
         <>
@@ -28,14 +30,15 @@ function HomePage() {
       ) : (
         <div>
           {<div>
-      <TriviaSelection onSelectDifficulty={handleSelectDifficulty} />     
-      {selectedUrl && <QuizApp triviaUrl={selectedUrl} />}
-    </div>}
-       
+            {clickSound()}
+            <TriviaSelection onSelectDifficulty={handleSelectDifficulty} />
+            {selectedUrl && <QuizApp triviaUrl={selectedUrl} />}
+          </div>}
+
         </div>
       )}
     </div>
- );
+  );
 }
 
 export default HomePage;
--- src/Components/Leaderboard/Leaderboard.jsx
+++ src/Components/Leaderboard/Leaderboard.jsx
@@ -2,40 +2,47 @@ import React from "react";
 import { readScoresFromLocalStorage } from "../../Lib/localStorage";
 import useSound from 'use-sound';
 import cheersSfx from '../../Sounds/CheerSound.mp3';
+import ClickSound from "../../Sounds/ButtonClickSound.wav";
 
 
 const Leaderboard = () => {
   let scoresPlayers = readScoresFromLocalStorage();
-  
-  const topFiveScores = scoresPlayers.slice(0, 5);
-  const [playCheers] = useSound(cheersSfx);
 
- function refreshPage() {
-  window.location.reload(false);
-}
+  const topFiveScores = scoresPlayers.slice(0, 5);
+
+  function refreshPage() {
+    window.location.reload(false);
+  }
+  const [playCheers] = useSound(cheersSfx);
+  const [clickSound] = useSound(ClickSound);
 
   return (
     <div>
       {playCheers()}
-       <img className="background-elements" src="./assets/2D-BG-Elements.png"></img>
-        <div class="columns">
-         <div class="column is-one-third">
+      <img className="background-elements" src="./assets/2D-BG-Elements.png"></img>
+      <div class="columns">
+        <div class="column is-one-third">
           <img src="\assets\left side cubes.svg"></img>
-          </div>
+        </div>
 
-<div class="column is-one-third">
-<img className="logo" src="/Logo.png"></img>
-      <h2>Leaderboard</h2>
-      <ul>
-        {topFiveScores.map((player, index) => (
-          <li key={index}>
-            {player.userName} - {player.score}
-          </li>
-        ))}
-      </ul>
-      <div><button className="btn" onClick={refreshPage}>Go Home</button></div>
-    </div>
-    </div>
+        <div class="column is-one-third">
+          <img className="logo" src="/Logo.png"></img>
+          <h2>Leaderboard</h2>
+          <ul>
+            {topFiveScores.map((player, index) => (
+              <li key={index}>
+                {player.userName} - {player.score}
+              </li>
+            ))}
+          </ul>
+          <div>
+            <button className="btn" onClick={() => {
+            clickSound(); // Play the click sound
+            setTimeout(refreshPage, 400);
+          }}>Go Home</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
