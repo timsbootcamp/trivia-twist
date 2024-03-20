6351e5849ead13ea14ad2fa3556bc06662eed36a
LeonieWhitehead <leoniewhitehead@hotmail.co.uk>
2024-03-20 19:21:53 +0000

    added comments to main jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
-import React from 'react';
-import HomePage from './Components/HomePage/HomePage.jsx'
-import './App.css'
+import React from 'react'; // Importing React library for JSX usage
+import HomePage from './Components/HomePage/HomePage.jsx'; // Importing the HomePage component
+import './App.css'; // Importing the CSS file for styling
 
-
-
+// Functional component for the main application
 function App() {
   return (
-  
-    <div className='app'>
-      
-    <HomePage />
+    <div className='app'> {/* Main container div with CSS class 'app' */}
+      <HomePage /> {/* Rendering the HomePage component */}
     </div>
   );
 }
 
-
-export default App;
+export default App; // Exporting the App component for use in other parts of the application
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react'; // Importing React library for JSX usage
+import ReactDOM from 'react-dom/client'; // Importing ReactDOM for rendering components
+import App from './App.jsx'; // Importing the main App component
 
+// Rendering the App component using ReactDOM.createRoot
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  <React.StrictMode> {/* Wrapping the App component with StrictMode for additional checks */}
+    <App /> {/* Rendering the main App component */}
+  </React.StrictMode>
+);
