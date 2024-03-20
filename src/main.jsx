c0d1712eb0fd85642f5de5417be55c53c88fd84b
LeonieWhitehead <leoniewhitehead@hotmail.co.uk>
2024-03-20 19:45:35 +0000

    removed all comments from main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,8 @@
-import React from 'react'; // Importing React library for JSX usage
-import ReactDOM from 'react-dom/client'; // Importing ReactDOM for rendering components
-import App from './App.jsx'; // Importing the main App component
-
-// Rendering the App component using ReactDOM.createRoot
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode> {/* Wrapping the App component with StrictMode for additional checks */}
-    <App /> {/* Rendering the main App component */}
-  </React.StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 )
