969de5ae7fafc8a6abf1778ba091f9f8492486ca
LeonieWhitehead <leoniewhitehead@hotmail.co.uk>
2024-03-20 19:43:13 +0000

    removed semi colon which was causing failure in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'; // Importing the main App component
 
 // Rendering the App component using ReactDOM.createRoot
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode> 
-    <App />
+  <React.StrictMode> {/* Wrapping the App component with StrictMode for additional checks */}
+    <App /> {/* Rendering the main App component */}
   </React.StrictMode>
-);
+)
