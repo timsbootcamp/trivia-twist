3d01711c36fc15d350bcce79f192cb5e9066ba00
nicosharpe <nicolesharpe11@gmail.com>
2024-03-20 23:13:14 +0000

    leaderboard styling

--- src/Components/Leaderboard/Leaderboard.jsx
+++ src/Components/Leaderboard/Leaderboard.jsx
@@ -3,6 +3,7 @@ import { readScoresFromLocalStorage } from "../../Lib/localStorage";
 import useSound from 'use-sound';
 import cheersSfx from '../../Sounds/CheerSound.mp3';
 import ClickSound from "../../Sounds/ButtonClickSound.wav";
+import './index.css'
 
 
 const Leaderboard = () => {
@@ -16,6 +17,9 @@ const Leaderboard = () => {
   const [playCheers] = useSound(cheersSfx);
   const [clickSound] = useSound(ClickSound);
 
+  //Array of file names for leaderboard badges
+  const badgeFiles = ["/assets/1st-icon.png", "/assets/2nd-icon.png", "/assets/3rd-icon.png", "/assets/4th-icon.png", "/assets/5th-icon.png"];
+
   return (
     <div className="leaderboard">
       {playCheers()}
@@ -27,22 +31,39 @@ const Leaderboard = () => {
 
         <div class="column is-one-third">
           <img className="logo" src="/Logo.png"></img>
-          <h2>Leaderboard</h2>
+
+         <div className="scoreContainer">
+          <h2 className="title ">Leaderboard</h2>
+          <container className="scores">
+            
           <ul>
             {topFiveScores.map((player, index) => (
-              <li key={index}>
+              <li key={index}
+              className="scoreDivs"
+              style={{
+                display: "block",
+                textAlign: "center",
+                border: "1px solid black",
+                padding: "10px",
+                marginBottom: "10px",
+                backgroundColor: "#FFF",
+              }}
+              >
+                  <img src={badgeFiles[index]} alt={`Badge for ${index + 1}`} className="leaderboardIcons" style={{ marginRight: "10px" }} /> {/* Display badge */}
                 {player.userName} - {player.score}
               </li>
             ))}
           </ul>
+          </container>
           <div>
-            <button className="btn" onClick={() => {
+            <button className="button" onClick={() => {
             clickSound(); // Play the click sound
             setTimeout(refreshPage, 400);
           }}>Go Home</button>
           </div>
         </div>
       </div>
+    </div>
     </div>
   );
 };
